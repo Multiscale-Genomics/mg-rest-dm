e00babd4c5ce2f1c2a569c7892b440192f4369c1
Mark McDowall <mark.mcdowall@gmail.com>
2016-11-28 16:45:31 +0000

    The REST app uses the APScheduler to test if REST end points are available

--- rest/app.py
+++ rest/app.py
@@ -16,11 +16,38 @@ limitations under the License.
 
 from flask import Flask, make_response, request
 from flask_restful import Api, Resource
+from flask_apscheduler import APScheduler
 
 from dmp import dmp
 
+import logging
+logging.basicConfig()
+
+class Config(object):
+    import json
+
+    with open('./registry.json') as data_file:    
+        data = json.load(data_file)
+    
+    JOBS = [
+        {
+            'id': 'ping',
+            'func': 'jobs:ping',
+            'args' : ("registry", data),
+            'trigger': 'interval',
+            'seconds': 3
+        }
+    ]
+
+    SCHEDULER_VIEWS_ENABLED = True
+
+
 app = Flask(__name__)
-api = Api(app)
+app.config.from_object(Config())
+
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
 
 
 class GetTracks(Resource):
@@ -43,6 +70,36 @@ class GetTracks(Resource):
             'files': files
         }
 
+
+class GetTrackHistory(Resource):
+    """
+    Class to handle the http requests for retrieving the list of file history of
+    a given file for a given user handle
+    """
+    
+    def get(self):
+        da = dmp()
+        
+        # TODO Placeholder code
+        user_id = request.args.get('user_id')
+        file_id = request.args.get('file_id')
+        files = da.get_files_history(file_id)
+        
+        return {
+            '_links': {
+                'self': request.base_url,
+            },
+            'history_files': files
+        }
+
+class ping(Resource):
+    """
+    Class to handle the http requests to ping a service
+    """
+    
+    def get(self):
+        return "alive"
+
 # TODO
 # For the services where there needs to be an extra layer (adjacency lists),
 # then there needs to be a way of forwarding for this. But the majority of
@@ -52,15 +109,23 @@ class GetTracks(Resource):
 """
 Define the URIs and their matching methods
 """
+api = Api(app)
+
 #   List the available species for which there are datasets available
 api.add_resource(GetTracks, "/rest/v0.0/getTracks", endpoint='tracks')
 
 #   List the available assemblies for a given species with links
 #api.add_resource(GetTrack, "/rest/v0.0/getTrack", endpoint='track')
 
+#   List file history
+api.add_resource(GetTrackHistory, "/rest/v0.0/getTrackHistory", endpoint='trackHistory')
+
+#   List file history
+api.add_resource(ping, "/rest/v0.0/dmp-ping", endpoint='dmp-ping')
+
 
 """
 Initialise the server
 """
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5001, debug=True, use_reloader=False)
